885320dc5f0e12e5d9c2dc276fbb10dcea1848f2
yhyeon <yhyeongyung@gmail.com>
2017-10-03 00:02:34 +0900

    Zip_Scanning

---
+++ zip_Scanning.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'silentlycontinue'
+
+[Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+$env:hostIP = (Get-NetIPConfiguration | Where-Object { $_.IPv4DefaultGateway -ne $null -and $_.netadapter.status -ne "Disconnected"}).ipv4address.ipaddress
+$env:hostMAC = (Get-NetAdapter | where-object -FilterScript {$_.Name -eq "이더넷"}).MacAddress
+get-psdrive | select root `
+|% {foreach($sourceFile in (Get-ChildItem $_.Root -recurse -file -Filter '*.zip'))
+{
+[IO.Compression.ZipFile]::OpenRead($sourceFile.FullName).Entries | %{$env:username + " : " + $env:userdomain + " : " + $env:COMPUTERNAME + " : " + $env:hostIP + " : " + $env:hostMAC + " : " + $sourcefile.CreationTime + " : " + $sourcefile.LastAccessTime + “ : “ + $sourcefile.lastwritetime + " : " + "{0:N2}" -f ($_.Length/1kb) + " : "  + $sourcefile.directoryName + " : " + "$sourcefile`: $($_.FullName): $($_.Length)";} | Out-File C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhhmm).txt -Append
+}}
