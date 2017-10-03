1985dbd311e56978bef79beba89bc68464f28f1f
yhyeon <hyeongyung@naver.com>
2017-10-03 17:36:23 +0900

    Update File_Scanning.ps1

--- File_Scanning.ps1
+++ File_Scanning.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'silentlycontinue'
 $env:hostIP = (Get-NetIPConfiguration | Where-Object { $_.IPv4DefaultGateway -ne $null -and $_.netadapter.status -ne "Disconnected"}).ipv4address.ipaddress
-$env:hostMAC = (Get-NetAdapter | where-object -FilterScript {$_.Name -eq "이더넷"}).MacAddress
+$env:hostMAC = (Get-NetAdapter | where-object -FilterScript {$_.Name -eq "이더넷" -or $_.Name -eq “Ethernet” -or $_.Name -eq “WI-FI”}).MacAddress
 get-psdrive | select root `
 | % {get-childitem $_.Root -file -recurse `
 | foreach {$env:username + " : " + $env:userdomain + " : " + $env:COMPUTERNAME + " : " + $env:hostIP + " : " + $env:hostMAC + " : " + $_.CreationTime + " : " + $_.LastAccessTime + “ : “ + $_.lastwritetime + " : " + "{0:N2}" -f ($_.Length/1kb) + " : " + $_.DirectoryName + " : " + $_.Name}  | Out-File  C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhhmm).txt -Append }
