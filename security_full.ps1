b3a4c201e28a655b354a7c10e94e177e0a063505
yhyeon <hyeongyung@naver.com>
2017-10-06 02:19:55 +0900

    Update security_full.ps1

--- security_full.ps1
+++ security_full.ps1
@@ -1,6 +1,10 @@
-get-winevent -FilterHashtable @{logname='security'; ID = 4663,4662,4656} -oldest| foreach {$_.toxml()}
+$events = get-winevent -FilterHashtable @{logname='security'; ID = 4663,4662,4656} -oldest | foreach {$_.toxml()}
 foreach ($security in $events)
 {$security = $security.split("<")
+$objecttype = $security | select-string -pattern 'ObjectType'
+$objecttype = $objecttype.line.Split("=").split(">")[2]
+if ($objecttype -eq "File")
+{
 $objectserver = $security | select-string -Pattern 'ObjectServer'
 $objectserver = $objectserver.line.Split("=").split(">")[2]
 if ($objectserver -eq "Security")
@@ -15,24 +19,49 @@ $sid = $security | select-string -pattern 'SubjectUserSid'
 $sid = $sid.line.split("=").split(">")[2]
 $username = $security | select-string -pattern 'SubjectUserName'
 $username = $username.line.Split("=").split(">")[2]
+$logonid = $security | select-string -pattern 'SubjectLogonId'
+$logonid = $logonid.line.Split("=").split(">")[2]
 $domainname = $security | select-string -pattern 'SubjectDomainName'
 $domainname = $domainname.line.Split("=").split(">")[2]
-$objecttype = $security | select-string -pattern 'ObjectType'
-$objecttype = $objecttype.line.Split("=").split(">")[2]
 $objectname = $security | select-string -pattern 'ObjectName'
 $objectname = $objectname.line.Split("=").split(">")[2]
-$handleid = $security | select-string -pattern 'HandleId'
-$handleid = $handleid.line.Split("=").split(">")[2]
-$accesslist = $security | select-string -pattern 'AccessList'
-$accesslist = $accesslist.line.Split("=").split(">").split("
-				")[2]
 $accessmask = $security | select-string -pattern 'AccessMask'
 $accessmask = $accessmask.line.Split("=").split(">")[2]
-$processid = $security | select-string -pattern 'ProcessId'
-$processid = $processid.line.Split("=").split(">")[6]
 $processname = $security | select-string -pattern 'ProcessName'
 $processname = $processname.line.Split("=").split(">")[2]
-$eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $domainname + " : " + $objectserver + " : " + $objecttype + " : " + $objectname + " : " + $handleid + " : " + $accesslist + " : " + $accessmask + " : " + $processid + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhhmm)_security.txt -Append }
+if($accessmask -eq "0x1")
+{
+$accessmask.replace("0x1","ReadData (or ListDirectory)") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+if($accessmask -eq "0x2")
+{
+$accessmask.replace("0x2","WriteData (or AddFile)") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname| out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+if($accessmask -eq "0x4")
+{
+$accessmask.replace("0x4","AppendData (or AddSubdirectory or CreatePipeInstance)") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+if($accessmask -eq "0x40")
+{
+$accessmask.replace("0x40","DeleteChild") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+if($accessmask -eq "0x80")
+{
+$accessmask.replace("0x80","ReadAttributes") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+if($accessmask -eq "0x100")
+{
+$accessmask.replace("0x100","WriteAttributes") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+if($accessmask -eq "0x10000")
+{
+$accessmaskt.replace("%%1537","DELETE") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+if($accessmask -eq "0x20000")
+{
+$accessmask.replace("0x20000","READ_CONTROL") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objectname + " : " + $processname | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security.txt -Append
+}
+}
 elseif ($objectserver -match "MTP")
 {
 $eventrecordid = $security | select-string -pattern 'EventRecordID>'
@@ -47,31 +76,27 @@ $sid = $security | select-string -pattern 'SubjectUserSid'
 $sid = $sid.line.split("=").split(">")[2]
 $username = $security | select-string -pattern 'SubjectUserName'
 $username = $username.line.Split("=").split(">")[2]
+$logonid = $security | select-string -pattern 'SubjectLogonId'
+$logonid = $logonid.line.Split("=").split(">")[2]
 $domainname = $security | select-string -pattern 'SubjectDomainName'
 $domainname = $domainname.line.Split("=").split(">")[2]
-$objecttype = $security | select-string -pattern 'ObjectType'
-$objecttype = $objecttype.line.Split("=").split(">")[2]
 $objectname = $security | select-string -pattern 'ObjectName'
 $objectname = $objectname.line.Split("=").split(">")[2]
 $operationtype = $security | select-string -pattern 'OperationType'
 $operationtype = $operationtype.line.Split("=").split(">")[2]
-$handleid = $security | select-string -pattern 'HandleId'
-$handleid = $handleid.line.Split("=").split(">")[2]
-$accesslist = $security | select-string -pattern 'AccessList'
-$accesslist = $accesslist
-$accesslist = $accesslist.line.Split("=").split(">").split("
-
-
-
-
-
-")
-$accesslist = $accesslist[2]+$accesslist[4]+$accesslist[6]+$accesslist[8]+$accesslist[10]+$accesslist[12]
 $accessmask = $security | select-string -pattern 'AccessMask'
 $accessmask = $accessmask.line.Split("=").split(">")[2]
-$additionalinfo = $security | select-string -Pattern 'AdditionalInfo'
-$additionalinfo = $additionalinfo.line.split("-").split(">")[1]
 $additionalinfo2 = $security | select-string -Pattern 'AdditionalInfo2'
 $additionalinfo2 = $additionalinfo2.line.split("-").split(">")[1]
-$eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $domainname + " : " + $objectserver + " : " + $objecttype + " : " + $operationtype + " : " + $handleid + " : " + $accesslist + " : " + $accessmask + " : " + $additionalinfo + " : " + $additionalinfo2 | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhhmm)_security.txt -Append}
+if($accessmask -eq "0x120116")
+{
+$accessmaskt.replace("0x120116","Write") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objecttype + " : " + $operationtype + " : " + $additionalinfo2  | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security_mtp.txt -Append
+}
+if($accessmask -eq "0x120089")
+{
+$accessmask.replace("0x120089","READ") + " : " + $eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objecttype + " : " + $operationtype + " : " + $additionalinfo2  | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security_mtp.txt -Append
+}
+$eventid + " : " + $time + " : " + $computer + " : " + $sid + " : " + $username + " : " + $logonid + " : " + $domainname + " : " + $objectserver + " : " + $objecttype + " : " + $operationtype + " : " + $handleid + " : " + $additionalinfo2 + " : " + $accessmask | out-file C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_security_mtp.txt -Append
+}
+}
 }
