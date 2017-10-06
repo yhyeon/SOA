b9462c26203e9ddc40b95cecb6e90e02d13f59cd
yhyeon <hyeongyung@naver.com>
2017-10-07 01:09:03 +0900

    Update Copy_ChromeHistoryFile.ps1

--- Copy_ChromeHistoryFile.ps1
+++ Copy_ChromeHistoryFile.ps1
@@ -1,1 +1,1 @@
-﻿Copy-Item 'C:\Users\*\AppData\Local\Google\Chrome\User Data\Default\History' -Destination C:\Users\Public\Documents\${env:CCOMPUTERNAME}_$(get-date -f yyyyMMddhh)_History
+﻿Copy-Item 'C:\Users\*\AppData\Local\Google\Chrome\User Data\Default\History' -Destination C:\Users\Public\Documents\${env:COMPUTERNAME}_$(get-date -f yyyyMMddhh)_History
