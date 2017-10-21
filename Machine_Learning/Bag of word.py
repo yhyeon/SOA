df18050b24c2f05f51bfabc6280a70995534397b
Googy <lkukh17@naver.com>
2017-10-21 18:39:16 +0900

    add korean encoding
    add korean encoding

---
+++ Machine_Learning/Bag of word.py
@@ -0,0 +1,8 @@
+import numpy
+import sys
+from sklearn.feature_extracion.text import CountVectorizer
+#pip install sklearn
+#pip install scipy
+
+
+count = CountVectorizer
--- reason_table/reason_table.py
+++ reason_table/reason_table.py
@@ -2,6 +2,7 @@ import pandas
 import pymysql
 import getpass
 import matplotlib.pylab as plt
+from matplotlib import font_manager, rc
 from pylab import figure, axes, pie, title, savefig
 import operator
 
@@ -21,10 +22,13 @@ DF = pandas.DataFrame(list(data), None, titles)
 #print(DF)
 
 con.close()
+# -------------------------------------------------------------------------------------------------     Pivot Table / Graph
+font_location = "C:\Windows\Fonts\malgunbd.ttf"
+font_name = font_manager.FontProperties(fname = font_location).get_name()
+rc('font', family = font_name)
 
 PT = pandas.pivot_table(DF, index = "department", columns = ["media"], values = "serious", aggfunc = "count")
-print(PT)
-
+#print(PT)
 
 PT.plot(kind = 'Bar')
 plt.ylabel("Count")
@@ -37,10 +41,11 @@ medias = list(set(DF["media"]))
 seriouses = list(set(DF["serious"]))
 PT_media = DF.pivot_table(columns = "media", values = "serious", aggfunc = "count")
 PT_serious = pandas.pivot_table(DF, columns = "serious", values = "media", aggfunc = "count")
-print(PT_media)
+#print(PT_media)
 for media in medias:
     print("media(%s) - " %media, end = "")
     print(PT_media[media][0])
+print("")
 for serious in seriouses:
     print("serious(%s) - " %serious, end = "")
     print(PT_serious[serious][0])
@@ -48,7 +53,7 @@ for serious in seriouses:
 parts = list(set(DF["department"]))
 part_top = {}
 PT = DF.pivot_table(columns = "department", values = "serious", aggfunc = "sum")
-print(PT)
+#print(PT)
 for i in range(len(parts)):
     part_top[parts[i]] = []
     part_top[parts[i]].append(PT[parts[i]][0])
