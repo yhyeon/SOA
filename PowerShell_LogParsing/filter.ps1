9005a10f09c1a9b311f17a7e0cd50298504abbf2
yhyeon <hyeongyung@naver.com>
2017-11-19 06:29:29 +0900

    Update filter.ps1

--- PowerShell_LogParsing/filter.ps1
+++ PowerShell_LogParsing/filter.ps1
@@ -7,11 +7,11 @@ if(!(test-path 'C:\ProgramData\soalog'))
 if (!(test-path "C:₩ProgramData₩soalog₩path.txt"))
 {
 $dpath = "C:\"
-(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName.replace("\","\/") | out-file C:\ProgramData\soalog\path.txt -Encoding utf8
-"C:\".replace("\","\/") | out-file C:\ProgramData\soalog\path.txt -Append
+(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName | out-file C:\ProgramData\soalog\path.txt -Encoding utf8
+"C:\" | out-file C:\ProgramData\soalog\path.txt -Append
 
 $fpath = "C:\ProgramData\soalog", "C:\Windows\soa", "C:\Users\$env:username\AppData\Local\Kakao", "C:\Program Files (x86)\Kakao\KakaoTalk"
-(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName.replace("\","\/") | out-file C:\ProgramData\soalog\path.txt -Append -Encoding utf8
+(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName | out-file C:\ProgramData\soalog\path.txt -Append -Encoding utf8
 $dpath.Dispose()
 $fpath.Dispose()
 }
@@ -19,11 +19,11 @@ else
 {
 $compare = Get-Content -Path C:\ProgramData\soalog\path.txt
 $dpath = "C:\"
-(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName.replace("\","\/") | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append -Encoding utf8
+(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append -Encoding utf8
 "C:\" | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append
 
 $fpath = "C:\ProgramData\soalog", "C:\Windows\soa", "C:\Users\$env:username\AppData\Local\Kakao", "C:\Program Files (x86)\Kakao\KakaoTalk"
-(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName.replace("\","\/") | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append -Encoding utf8
+(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append -Encoding utf8
 $dpath.Dispose()
 $fpath.Dispose()
 }
