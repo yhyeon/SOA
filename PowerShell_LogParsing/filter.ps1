11f220fb50d8e5820232f559e508ef01fc5bd50d
yhyeon <hyeongyung@naver.com>
2017-11-19 05:42:58 +0900

    Update filter.ps1

--- PowerShell_LogParsing/filter.ps1
+++ PowerShell_LogParsing/filter.ps1
@@ -1,4 +1,4 @@
-﻿#$user = $env:USERNAME
+#$user = $env:USERNAME
 #$path = "C:\Program Files", "C:\Program Files (x86)", "C:\ProgramData", "C:\Users\$user\IntelGraphicsProfiles", "C:\Windows"
 $ErrorActionPreference = 'silentlycontinue'
 if(!(test-path 'C:\ProgramData\soalog'))
@@ -7,11 +7,11 @@ if(!(test-path 'C:\ProgramData\soalog'))
 if (!(test-path "C:₩ProgramData₩soalog₩path.txt"))
 {
 $dpath = "C:\"
-(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName | out-file C:\ProgramData\soalog\path.txt
-"C:\" | out-file C:\ProgramData\soalog\path.txt -Append
+(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName.replace("\","\/") | out-file C:\ProgramData\soalog\path.txt
+"C:\".replace("\","\/") | out-file C:\ProgramData\soalog\path.txt -Append
 
 $fpath = "C:\ProgramData\soalog", "C:\Windows\soa", "C:\Users\$env:username\AppData\Local\Kakao", "C:\Program Files (x86)\Kakao\KakaoTalk"
-(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName | out-file C:\ProgramData\soalog\path.txt -Append
+(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName.replace("\","\/") | out-file C:\ProgramData\soalog\path.txt -Append
 $dpath.Dispose()
 $fpath.Dispose()
 }
@@ -19,11 +19,11 @@ else
 {
 $compare = Get-Content -Path C:\ProgramData\soalog\path.txt
 $dpath = "C:\"
-(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append
+(Get-ChildItem -Path $dpath -recurse -Directory | select Fullname).FullName.replace("\","\/") | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append
 "C:\" | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append
 
 $fpath = "C:\ProgramData\soalog", "C:\Windows\soa", "C:\Users\$env:username\AppData\Local\Kakao", "C:\Program Files (x86)\Kakao\KakaoTalk"
-(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append
+(Get-ChildItem -Path $fpath -recurse -file | select Fullname).FullName.replace("\","\/") | Where-Object {$_ -notin $compare} | out-file C:\ProgramData\soalog\path.txt -Append
 $dpath.Dispose()
 $fpath.Dispose()
 }
