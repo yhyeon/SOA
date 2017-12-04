c4fd0c54434980838d9ae1e6372ca52141328906
yhyeon <yhyeongyung@gmail.com>
2017-12-05 04:53:10 +0900

    usnjrnl using powerforensics

---
+++ PowerShell_LogParsing/usnjrnl.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'silentlycontinue'
+
+Import-Module PowerForensics
+$uj = (Get-ForensicUsnJrnl -VolumeName C:)
+$ujs = $uj | select volumepath, recordnumber, filesequencenumber, parentfilerecordnumber, parentfilesequencenumber, usn, timestamp, reason, filename, fileattributes
+$count = $uj.Count
+for($i = 0; $i -lt $count; $i++)
+{
+$timestamp = $ujs[$i].timestamp | Get-Date -Format yyyy-MM-ddTHH:mm:ss+09:00
+[string]($ujs[$i].VolumePath) + ":::;" + [string]($ujs[$i].RecordNumber)  + ":::;" + [string]($ujs[$i].FileSequenceNumber) + ":::;" + [string]($ujs[$i].ParentFileRecordNumber) + ":::;" + [string]($ujs[$i].ParentFileSequenceNumber) + ":::;" + [string]($ujs[$i].Usn) + ":::;" + $timestamp + ":::;" + [string]($ujs[$i].Reason) + ":::;" + [string]($ujs[$i].FileName) + ":::;" + [string]($ujs[$i].FileAttributes) | select -Unique | Out-File C:\ProgramData\soalog\${sn}_$(get-date -f yyyyMMddHHmmss)_usnjrnl.txt -Append -Encoding utf8
+$i.Dispose()
+}
+$count.Dispose()
+$ujs.Dispose()
+$uj.Dispose()
